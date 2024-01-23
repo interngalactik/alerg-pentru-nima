37fba9324d7d8f6225b26c910255d106d1121e46
interngalactik <eduard.nistru@gmail.com>
2024-01-23 20:45:03 +0200

    Added a way to update the number of donors through csv google sheets

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,48 @@ import Header from '../components/header'
 import RacesTable from '../components/races-table'
 import AboutCampaign from '../components/aboutCampaign'
 import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node
   const morePosts = edges.slice(1)
 
-  const donors = 670;
+  const [csvData, setCsvData ] = useState([]);
+
+  useEffect(() => {
+      fetchCsvData()
+  }, []);
+
+  const fetchCsvData = () => {
+      const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR5EHXb8xjUUUTRehRkuMXXKMOlU-VvGTSNMhFBhrHGlcraKkmPfq9hZQb7aTYcgm6_MWjye0BZXNVf/pub?output=csv';
+
+      axios.get(csvUrl)
+      .then((response) => {
+          const parsedCsvData = parseCsv(response.data);
+          setCsvData(parsedCsvData);
+      })
+      .catch((error) => {
+          console.error('Error fetching CSV data: ', error)
+      })
+  }
+
+  function parseCsv(csvText) {
+      const rows = csvText.split(/\r?\n/);        // Use a regular expression to split the CSV text into rows while handling '\r'
+      const headers = rows[0].split(',');        // Extract headers (assumes the first row is the header row)
+      const data = [];        // Initialize an array to store the parsed data
+      for (let i = 1; i < rows.length; i++) {
+          const rowData = rows[i].split(',');          // Use the regular expression to split the row while handling '\r'
+          const rowObject = {};
+          for (let j = 0; j < headers.length; j++) {
+              rowObject[headers[j]] = rowData[j];
+          }
+          data.push(rowObject);
+      }
+      return data;
+  }
+  
+  const donors = csvData[0]?.currentlyDonors;
 
   return (
     <Layout preview={preview}>
