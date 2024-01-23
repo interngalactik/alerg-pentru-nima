f883d46e7a1487f4ac20d31bc503d3ee664d8ef8
interngalactik <eduard.nistru@gmail.com>
2024-01-23 21:45:55 +0200

    updated intro text

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ export default function Intro({donors}) {
   return (
     <section className="flex-col md:flex-col flex md:justify-between mt-16 mb-16 md:mb-12 text-white">
       <h4 className="text-2xl font-medium mb-8">
-        Susține <a href="https://sanctuarnima.ro" className="underline hover:text-green-700 duration-200 transition-colors" target='_blank'>Sanctuarul Nima</a> alături de alte <span className="text-green-700 font-bold">{donors}</span> de persoane.
+        Susține <a href="https://sanctuarnima.ro" className="underline hover:text-green-700 duration-200 transition-colors" target='_blank'>Sanctuarul Nima</a> alături de alte <span className="text-green-700 font-bold">{donors}</span> persoane.
       </h4>
       <h4 className="text-4xl md:text-4xl font-medium tracking-tighter leading-tight md:pr-8">
         Donează <span className='text-green-700 font-semibold'>2 euro / lună</span>
